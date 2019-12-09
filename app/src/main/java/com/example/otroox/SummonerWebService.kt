d7ee35b2efe68e354e9f35a1162a92a35ea18939
Jeh0va <georgesandalous@gmail.com>
2019-12-09 11:36:27 +0100

    MAJ de la APIKEY

--- app/src/main/java/com/example/otroox/SummonerWebService.kt
+++ app/src/main/java/com/example/otroox/SummonerWebService.kt
@@ -5,8 +5,8 @@ import retrofit2.http.GET
 
 interface SummonerWebService {
 
-        @GET("/lol/summoner/v4/summoners/by-name/Aatrlx?api_key=RGAPI-21698a6d-0aa1-4fed-b7ae-0322a7a12290")
-        fun getRandomCocktail(): Call<Sumoner>
+@GET("/lol/summoner/v4/summoners/by-name/Aatrlx?api_key=RGAPI-1536becf-de8b-4084-8f73-ac19ee1a810c")
+fun getRandomCocktail(): Call<Sumoner>
 
 }
 
