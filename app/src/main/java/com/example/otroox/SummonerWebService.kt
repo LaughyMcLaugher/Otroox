2d98803f97511e12b8fe676eaee63208be449cf2
Jeh0va <georgesandalous@gmail.com>
2019-12-25 13:10:39 +0100

    Ammélioration visuel

--- app/src/main/java/com/example/otroox/SummonerWebService.kt
+++ app/src/main/java/com/example/otroox/SummonerWebService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Path
 
 interface SummonerWebService {
 
-        @GET("/lol/summoner/v4/summoners/by-name/{pseudo}?api_key=RGAPI-1536becf-de8b-4084-8f73-ac19ee1a810c")
+        @GET("/lol/summoner/v4/summoners/by-name/{pseudo}?api_key=RGAPI-5d640626-45ce-41eb-b7fd-661b20e3f495")
         fun getInvocateur(@Path("pseudo")pseudo:String): Call<Sumoner>
 
 }
